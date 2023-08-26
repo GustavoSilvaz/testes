99f7136fb5b9ba72cc33ada582bbcec099579278
Gustavo Silva <guh_sz@outlook.com>
2023-08-26 16:37:33 -0300

    coisa pakas

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 var h = new Date();
 var hora = h.getHours();
+var nome = document.getElementById("hour");
+var c = document.getElementById("cor")
+var ig=document.getElementById("imgd")
+
 
 if (hora<12){
-    var nome =document.getElementById("hour");
     nome.innerHTML=`Tenha um bom dia`;
-    
+    c.style.backgroundColor="gray";
+    ig.innerHTML=`<img src="Amanhecer01.jpg" alt="">`;
 } else if (hora<18) {
-    var nome =document.getElementById("hour");
     nome.innerHTML=`Tenha uma boa tarde`; 
-
+    c.style.backgroundColor="orange";
+    ig.innerHTML=`<img src="pexels-photo-2386144.jpg" alt="">`
 } else if (hora<24){
-    var nome =document.getElementById("hour");
     nome.innerHTML=`Tenha uma boa noite.`;
+    c.style.backgroundColor="blue";
+    ig.innerHTML=`<img src="images.jpeg" alt="">`
 }
